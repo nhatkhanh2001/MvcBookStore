0904e82c3ea9a4977d9320a48840bbe5247ba2bb
Hồng Nhật Khánh <nhatkhanhcm2001@gmail.com>
2022-03-17 11:24:38 +0700

    Add project files.

---
+++ MvcBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcBookStore.Models;
+
+namespace MvcBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        dbQLBanSachDataContext data = new dbQLBanSachDataContext();
+        // GET: MvcBookStore
+        private List<SACH> Laysachmoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+
+        public ActionResult Index()
+        {
+            var sachmoi = Laysachmoi(5);    
+            return View(sachmoi);
+        }
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+
+        public ActionResult Nhaxuatban()
+        {
+            var chude = from cd in data.NHAXUATBANs select cd;
+            return PartialView(chude);
+        }
+        public ActionResult SPTheoChude(int id)
+        {
+            var sach = from s in data.SACHes where s.MaCD == id select s;
+            return View(sach);
+        }
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return View(sach);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes
+                       where s.Masach == id
+                       select s;
+            return View(sach.Single());
+        }
+    }
+}
---
+++ MvcBookStore/Controllers/NguoiDungController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBookStore.Controllers
+{
+    public class NguoiDungController : Controller
+    {
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dangky()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, KHACHHANG kh)
+        {
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var hoten = collection["HotenKH"];
+            var hoten = collection["HotenKH"];
+            var hoten = collection["HotenKH"];
+            var hoten = collection["HotenKH"];
+            var hoten = collection["HotenKH"];
+        }
+    }
+}
